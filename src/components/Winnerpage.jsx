562e30d572f11ad88ab10774a80d229f68157d74
thidayu <tanthidayu_0000@hotmail.com>
2024-12-13 08:29:00 +0700

    edit style

--- src/components/Winnerpage.jsx
+++ src/components/Winnerpage.jsx
@@ -66,7 +66,7 @@ const Winnerpage = () => {
         color: "black",
         position: "relative",
         overflow: "hidden",
-        backgroundImage: "url('BG_Luckdraw.png')",
+        backgroundImage: "url('BG_Luckdraw3.png')",
         backgroundSize: "100% 100%",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
@@ -92,113 +92,119 @@ const Winnerpage = () => {
           <ArrowBackIosNewIcon />
         </IconButton>
 
-        {/* Content container */}
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-end",
-            gap: 0,
-            pr: { xs: 4, sm: 6, md: 8 },
-            mt: 30,
-          }}
-        >
-          {/* Name and Sale container */}
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              width: "58%", // This creates a box that's half the width
-            }}
-          >
-            {/* Prize Name */}
-            <Typography
-              variant="h5"
-              sx={{
-                fontFamily: "DBOzoneXBold",
-                fontWeight: "bold",
-                fontSize: { xs: "1.2rem", sm: "1.5rem", md: "4.5rem" },
-                // color: "#0C3C6F",
-                color: "white",
-                textShadow: "#2F2F2F 1px 0 8px",
-                width: 1,
-                mb: 0,
-                mt: 2,
-              }}
-            >
-              รางวัล {getPrizeDisplay(prizeName.split(" (")[0])}
-            </Typography>
-
-            {/* Name */}
-            <Typography
-              sx={{
-                fontWeight: "bold",
-                fontFamily: "DBOzoneXBold",
-                fontSize: { xs: "1.2rem", sm: "1.5rem", md: "4rem" },
-                color: "white",
-                textAlign: "center",
-                width: "100%",
-                textShadow: "#2F2F2F 1px 0 8px",
-                mb: 0,
-                mt: 0,
-              }}
-            >
-              คุณ {userData.firstname} {userData.lastname}
-            </Typography>
-            {/* Sale (centered within the box) */}
-            <Typography
-              sx={{
-                fontWeight: "bold",
-                fontFamily: "DBOzoneXBold",
-                fontSize: { xs: "1.2rem", sm: "1.5rem", md: "4rem" },
-                color: "white",
-                textAlign: "center", // Center the sale text
-                width: "100%",
-                textShadow: "#2F2F2F 1px 0 8px",
-                mb: 0,
-                mt: -4,
-              }}
-            >
-              {userData.Sale?.toString().replaceAll("B2B_", "")}
-            </Typography>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <Box sx={{ width: "5%",mt:20,ml:-15,mr:10 }}>
+            <img src="Box1.png" />
           </Box>
 
-          {/* Ticket Number */}
-
+          {/* Content container */}
           <Box
             sx={{
-              width: "60%",
-              aspectRatio: "2 / 1",
+              width: "100%",
               display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundImage: "url('Ticket.png')",
-              backgroundSize: "130% 130%",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              textShadow: "4px 4px 8px rgba(0,0,0,0.5)",
-              mt: -12,
-              pr: 20,
+              flexDirection: "column",
+              alignItems: "flex-end",
+              gap: 0,
+              pr: { xs: 4, sm: 6, md: 8 },
+              mt: 30,
             }}
           >
-            <Typography
-              variant="h3"
+            {/* Name and Sale container */}
+            <Box
               sx={{
-                fontWeight: "bold",
-                fontFamily: "DBOzoneXBold",
-                letterSpacing: "8px",
-                fontSize: { xs: "2rem", sm: "2.5rem", md: "7rem" },
-                color: "white",
-                mt: 0,
-                mb: 1,
-                alignSelf: "center",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "58%", // This creates a box that's half the width
               }}
             >
-              {ticketNumber}
-            </Typography>
+              {/* Prize Name */}
+              <Typography
+                variant="h5"
+                sx={{
+                  fontFamily: "DBOzoneXBold",
+                  fontWeight: "bold",
+                  fontSize: { xs: "1.2rem", sm: "1.5rem", md: "4.4rem" },
+                  // color: "#0C3C6F",
+                  color: "white",
+                  textShadow: "#2F2F2F 1px 0 8px",
+                  width: 1,
+                  mb: 0,
+                  mt: 4,
+                }}
+              >
+                รางวัล {getPrizeDisplay(prizeName.split(" (")[0])}
+              </Typography>
+
+              {/* Name */}
+              <Typography
+                sx={{
+                  fontWeight: "bold",
+                  fontFamily: "DBOzoneXBold",
+                  fontSize: { xs: "1.2rem", sm: "1.5rem", md: "4rem" },
+                  color: "white",
+                  textAlign: "center",
+                  width: "100%",
+                  textShadow: "#2F2F2F 1px 0 8px",
+                  mb: 0,
+                  mt: 0,
+                }}
+              >
+                คุณ {userData.firstname} {userData.lastname}
+              </Typography>
+              {/* Sale (centered within the box) */}
+              <Typography
+                sx={{
+                  fontWeight: "bold",
+                  fontFamily: "DBOzoneXBold",
+                  fontSize: { xs: "1.2rem", sm: "1.5rem", md: "4rem" },
+                  color: "white",
+                  textAlign: "center", // Center the sale text
+                  width: "100%",
+                  textShadow: "#2F2F2F 1px 0 8px",
+                  mb: 0,
+                  mt: -4,
+                }}
+              >
+                {userData.Sale?.toString().replaceAll("B2B_", "")}
+              </Typography>
+            </Box>
+
+            {/* Ticket Number */}
+
+            <Box
+              sx={{
+                width: "60%",
+                aspectRatio: "2 / 1",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundImage: "url('Ticket.png')",
+                backgroundSize: "130% 130%",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                textShadow: "4px 4px 8px rgba(0,0,0,0.5)",
+                mt: -12,
+                pr: 20,
+              }}
+            >
+              <Typography
+                variant="h3"
+                sx={{
+                  fontWeight: "bold",
+                  fontFamily: "DBOzoneXBold",
+                  letterSpacing: "8px",
+                  fontSize: { xs: "2rem", sm: "2.5rem", md: "7rem" },
+                  color: "white",
+                  mt: 0,
+                  mb: 1,
+                  alignSelf: "center",
+                }}
+              >
+                {ticketNumber}
+              </Typography>
+            </Box>
           </Box>
         </Box>
       </Box>
